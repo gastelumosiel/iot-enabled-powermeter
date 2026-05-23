890a703ed74e607a18c2c0b9190dfac878cbc0d7
Rene Lozano <rene.lozano@cetys.edu.mx>
2026-05-22 20:23:21 -0700

    edit8

--- backend/pahomqtt/views.py
+++ backend/pahomqtt/views.py
@@ -16,6 +16,8 @@ def publish_message(request):
     return JsonResponse({'code':rc})
 
 if message_flag is True:
+    message_flag = False
+    print("Saving message")
     if not isinstance(message_received, str):
         raise TypeError("Input must be a JSON string.")
     msg = json.loads(message_received)
