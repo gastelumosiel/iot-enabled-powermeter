45f37e01a5bc2db12565ae68c52c9358728e0131
Rene Lozano <rene.lozano@cetys.edu.mx>
2026-05-22 18:27:47 -0700

    edit2

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = []
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
 }
 
 # Application definition
