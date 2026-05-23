217fc30a9dbbf528fec0555c6152f15cc4e01444
Rene Lozano <rene.lozano@cetys.edu.mx>
2026-05-22 20:40:27 -0700

    edit10

--- backend/pahomqtt/apps.py
+++ backend/pahomqtt/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class PahomqttConfig(AppConfig):
     name = 'pahomqtt'
+
+    def ready(self):
+        if getattr(self, '_mqtt_started', False):
+            return
+
+        import mysite.mqtt as mqtt
+        mqtt.client.loop_start()
+        self._mqtt_started = True
