4b13d29dcade9417f4f5df4ec2c85ff6866d6a24
Rene Lozano <rene.lozano@cetys.edu.mx>
2026-06-05 00:06:53 -0700

    Fix login 2

--- powerfrontend/src/services/api.js
+++ powerfrontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? (import.meta.env.PROD ? '' : 'http://localhost:8000'),
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? (import.meta.env.PROD ? '' : 'https://powerlytix.org/api'),
   timeout: 1800,
 })
 
