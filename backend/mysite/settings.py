8b350f637109eba6b0fe2679ad9a9d1670a56634
Rene Lozano <rene.lozano@cetys.edu.mx>
2026-06-04 17:42:16 -0700

    Changed DB to MySQL

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PowerlytixDB',
+        'USER': 'server',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
