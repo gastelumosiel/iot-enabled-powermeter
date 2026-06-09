702e6c9bf972af2f3a44045b2aa300019046f95e
Rene Lozano <rene.lozano@cetys.edu.mx>
2026-06-08 17:35:36 -0700

    small fix 2

--- powerfrontend/src/services/api.js
+++ powerfrontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? (import.meta.env.PROD ? '' : 'https://powerlytix.org'),
-  timeout: 1800,
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? '',
+  timeout: 10000,
 })
 
 api.interceptors.request.use((config) => {
